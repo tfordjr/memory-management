d60adc1082acae5637fddb85ed85ee60cf805e5b
tfordjr <98005445+tfordjr@users.noreply.github.com>
2024-02-03 12:05:57 -0600

    updated user.cpp workflow

--- user.cpp
+++ user.cpp
@@ -11,9 +11,9 @@ using namespace std;
 int main(int argc, char** argv) {
     int iterations = atoi(argv[1]);
     for(int i = 1; i < iterations + 1; i++){
-        printf("USER PID: %d  PPID: %d  Iteration: %d\n", getpid(), getppid(), i);       
-    }    
-
-    sleep(3);
+        printf("USER PID: %d  PPID: %d  Iteration: %d before sleeping\n", getpid(), getppid(), i);  
+        sleep(1);
+        printf("USER PID: %d  PPID: %d  Iteration: %d after sleeping\n" , getpid(), getppid(), i);  
+    }        
     return EXIT_SUCCESS; 
 }
