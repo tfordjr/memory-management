f1dab701d0eb578dd0dda622ec5dcbbb483db3ca
tfordjr <98005445+tfordjr@users.noreply.github.com>
2024-02-12 20:20:40 -0600

    .

--- oss.cpp
+++ oss.cpp
@@ -60,7 +60,7 @@ int main(int argc, char** argv){
 
     std::signal(SIGALRM, timeout_handler);  // signal handlers setup
     std::signal(SIGINT, ctrl_c_handler);
-    alarm(6);
+    alarm(60);
 
     Clock* clock;       // init shm clock
     key_t key = ftok("/tmp", 35);
@@ -124,7 +124,7 @@ void print_process_table(PCB processTable[], int simultaneous, int secs, int nan
     static int next_print_nanos = 0;   // process table print to be done
 
     if(secs > next_print_secs || secs == next_print_secs && nanos > next_print_nanos){
-        printf("OSS PID: %d  SysClockS: %d  SysClockNano: %d  \nProcess Table:\nEntry\tOccupied\tPID\tStartS\t\tStartN\n", getpid(), secs, nanos);
+        printf("OSS PID: %d  SysClockS: %d  SysClockNano: %d  \nProcess Table:\nEntry  \tOccupied   PID\tStartS\tStartN\n", getpid(), secs, nanos);
         for(int i = 0; i < simultaneous; i++){
             printf("%d\t%d\t%d\t%d\t%d\n", i, processTable[i].occupied, processTable[i].pid, processTable[i].startSecs, processTable[i].startNanos);
         }
