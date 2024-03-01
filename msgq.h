1b6be314270d77f91013ac659ab4b0de75a7e848
tfordjr <98005445+tfordjr@users.noreply.github.com>
2024-03-01 11:36:45 -0600

    offloaded msgbuffer struct to msgq.h

---
+++ msgq.h
@@ -0,0 +1,26 @@
+// CS4760-001SS - Terry Ford Jr. - Project 3 Message Queues - 02/29/2024
+// https://github.com/tfordjr/message-queues.git
+
+#ifndef MSGQ_H
+#define MSGQ_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+
+typedef struct msgbuffer {   // Had to change names, I was getting confused
+	pid_t address;           // type pid_t again so that I avoid confusion
+	char message[100];
+	int msgCode;
+} msgbuffer;
+
+#define MSG_TYPE_SUCCESS 1  // I'm getting confused, so I'm implementing these 
+#define MSG_TYPE_RUNNING 0  
+
+#endif
