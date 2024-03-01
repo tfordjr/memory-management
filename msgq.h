cac1ab66edc1a94351efbd7a3b1d168b020c212b
tfordjr <98005445+tfordjr@users.noreply.github.com>
2024-03-01 13:46:41 -0600

    trying to sync msgqueue

--- msgq.h
+++ msgq.h
@@ -14,8 +14,8 @@
 #include <sys/msg.h>
 #include <unistd.h>
 
-#define MSGQ_FILE_PATH "msgq.txt"
-#define MSGQ_PROJ_ID 1
+#define MSGQ_FILE_PATH "/var/tmp"
+#define MSGQ_PROJ_ID 55
 #define PERMS 0644
 
 typedef struct msgbuffer {   // Had to change names, I was getting confused
