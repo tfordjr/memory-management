0a8ec4023870082d1616b108882c2a9f795e5295
tfordjr <98005445+tfordjr@users.noreply.github.com>
2024-03-10 17:45:22 -0500

    trying to prevent stale rcvbuf data in user.cpp

--- scheduler.h
+++ scheduler.h
@@ -33,6 +33,7 @@ void scheduler(PCB processTable[], int simultaneous, int *i, int *time_slice, in
     }
 
     pid_t pid;  
+    *time_slice = 0;
        
     if(!Q0.empty()){   // THIS IS SCHEDULING BLOCKED PROCS, right????
         pid = Q0.front();
