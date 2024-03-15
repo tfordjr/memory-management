6195f84baea2385fe5c3bc40246e4d94e5869c65
tfordjr <98005445+tfordjr@users.noreply.github.com>
2024-03-15 17:58:49 -0500

    adjusting stats

--- oss.cpp
+++ oss.cpp
@@ -273,13 +273,13 @@ void output_statistics(int totalChildren, double totalTimeInSystem, double total
     double totalWaitTime = totalTimeInSystem - (totalBlockedTime + totalCPUTime);
     std::cout << "\nRUN RESULT REPORT" << std::endl;
     std::cout << std::fixed << std::setprecision(2) << "Average Wait Time: " << totalWaitTime/totalChildren << " seconds" << std::endl;   
-    std::cout << "Average CPU Utilization: " << (totalCPUTime/totalTimeInSystem)*100 << "%" << std::endl;           
+    std::cout << "Average CPU Utilization: " << (totalCPUTime/totalClockTime)*100 << "%" << std::endl;           
     std::cout << "Average Blocked Time: " << totalBlockedTime/totalChildren << " seconds" << std::endl; 
     std::cout << "Total Idle CPU Time: " << totalClockTime - totalCPUTime << " seconds\n" << std::endl;
 
     outputFile << "\nRUN RESULT REPORT" << std::endl;
     outputFile << std::fixed << std::setprecision(2) << "Average Wait Time: " << totalWaitTime/totalChildren << " seconds" << std::endl;      
-    outputFile << "Average CPU Utilization: " << (totalCPUTime/totalTimeInSystem)*100 << "%" << std::endl;          
+    outputFile << "Average CPU Utilization: " << (totalCPUTime/totalClockTime)*100 << "%" << std::endl;          
     outputFile << "Average Blocked Time: " << totalBlockedTime/totalChildren << " seconds" << std::endl; 
     outputFile << "Total Idle CPU Time: " << totalClockTime - totalCPUTime << " seconds\n" << std::endl;
 }
