c6e4d57e82c164d46d128c26238b2307c037c85c
tfordjr <98005445+tfordjr@users.noreply.github.com>
2024-03-15 16:03:07 -0500

    added run result report

--- oss.cpp
+++ oss.cpp
@@ -270,12 +270,13 @@ void cleanup(string cause) {
 void output_statistics(int totalChildren, double totalTimeInSystem, double totalBlockedTime, double totalCPUTime){
     double totalClockTime = shm_clock->secs + (shm_clock->nanos)/1e9;
     double totalWaitTime = totalTimeInSystem - (totalBlockedTime + totalCPUTime);
-
+    std::cout << "\nRUN RESULT REPORT" << std::endl;
     std::cout << "Average Wait Time: " << totalWaitTime/totalChildren << " seconds" << std::endl;   
     std::cout << "Average CPU Utilization: " << totalCPUTime/totalChildren << " seconds" << std::endl;           
     std::cout << "Average Blocked Time: " << totalBlockedTime/totalChildren << " seconds" << std::endl; 
     std::cout << "Total Idle CPU Time: " << totalClockTime - totalCPUTime << " seconds" << std::endl;
 
+    outputFile << "\nRUN RESULT REPORT" << std::endl;
     outputFile << "Average Wait Time: " << totalWaitTime/totalChildren << " seconds" << std::endl;      
     outputFile << "Average CPU Utilization: " << totalCPUTime/totalChildren << " seconds" << std::endl;          
     outputFile << "Average Blocked Time: " << totalBlockedTime/totalChildren << " seconds" << std::endl; 
