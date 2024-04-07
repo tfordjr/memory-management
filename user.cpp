7469009d41a7589b1c054ed4486e21df441642f7
tfordjr <98005445+tfordjr@users.noreply.github.com>
2024-04-07 12:06:00 -0500

    clean up user.cpp

--- user.cpp
+++ user.cpp
@@ -28,9 +28,6 @@ using namespace std;
       // Remaining (100 - (TERM_CHANCE + R_REQUEST_CHANCE)) = R_RELEASE_CHANCE
 #define R_INTERVAL_BOUND 1e7  // max bound is 10 ms (in ns)
 
-void calculate_time_until_unblocked(int, int, int, int *, int *);
-bool will_process_terminate_during_quantum(int, int, int, int, int, int, int, int*);
-
 int main(int argc, char** argv) {
     Clock* shm_clock;           // init shm clock
 	key_t clock_key = ftok("/tmp", 35);
@@ -109,42 +106,4 @@ int main(int argc, char** argv) {
     shmdt(shm_clock);  // deallocate shm and terminate
     printf("%d: Child is terminating...\n",getpid());
     return EXIT_SUCCESS; 
-}
-
-void calculate_time_until_unblocked(int secs, int nanos, int nanos_blocked, int *temp_unblock_secs, int *temp_unblock_nanos){
-    nanos += nanos_blocked;
-    if (nanos >= 1000000000){   // if over 1 billion nanos, add 1 second, sub 1 bil nanos
-        nanos -= 1000000000;
-        secs++;
-    }
-    *temp_unblock_secs = secs;
-    *temp_unblock_nanos = nanos;
-    return;
-}
-
-bool will_process_terminate_during_quantum(int secs, int nanos, int end_secs, int end_nanos, int quantum, int secs_plus_quantum, int nanos_plus_quantum, int* time_slice_used){
-    secs_plus_quantum = secs;
-    nanos_plus_quantum = nanos;
-    
-    nanos_plus_quantum += quantum;
-
-    if (nanos >= 1000000000){   // if over 1 billion nanos, add 1 second, sub 1 bil nanos
-        nanos_plus_quantum -= 1000000000;
-        secs_plus_quantum++;
-    }
-    
-    int elapsed_secs = end_secs - secs_plus_quantum;  // difference between termination time and clock time
-    int elapsed_nanos = end_nanos - nanos_plus_quantum;    
-   
-    if (elapsed_nanos < 0) {  
-        elapsed_secs--;       
-        elapsed_nanos += 1000000000;
-    }
-        // if difference is negative, process will end during quantum
-    if(elapsed_secs < 0){
-        *time_slice_used = abs(elapsed_secs * 1000000000 + elapsed_nanos);
-        return true;
-    }
-    
-    return false;
 }
