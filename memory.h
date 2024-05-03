33cbd82e85de1402d50457dcb0a5d03b3862e628
tfordjr <98005445+tfordjr@users.noreply.github.com>
2024-05-03 13:50:06 -0500

    added oss memory functions

--- memory.h
+++ memory.h
@@ -1,6 +1,24 @@
 // CS4760-001SS - Terry Ford Jr. - Project 6 Memory Management - 04/21/2024
 // https://github.com/tfordjr/memory-management.git
 
-// memory table structs
+#ifndef MEMORY_H
+#define MEMORY_H
 
-// init memory tables
+struct frame{    // OSS FRAME TABLE - 256K memory, 256 1k frames
+    bool secondChanceBit;
+    bool dirtyBit;
+};
+
+void page_fault(){
+    static int victimFrame = 0;
+    // run second chance algorithm, determine which frame will be replaced
+    // victimFrame = replaced frame + 1
+}
+
+void page_request(){
+    // If page is not in page table (main memory) then page fault
+}
+
+
+
+#endif
