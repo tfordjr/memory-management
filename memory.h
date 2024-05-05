03f55c5eb9f922407226ebfebfd5b1d4f1898801
tfordjr <98005445+tfordjr@users.noreply.github.com>
2024-05-05 17:07:03 -0500

    adding tons of feedback to stdout and logfile

--- memory.h
+++ memory.h
@@ -59,8 +59,8 @@ void print_frame_table(Page frameTable[], int secs, int nanos, std::ostream& out
         std::cout << "OSS PID: " << getpid() << "  SysClockS: " << secs << "  SysClockNano " << nanos << "  \nPage Table:\n\tOwner PID\tPage Number\t2nd Chance Bit\tDirty Bit\n";
         outputFile << "OSS PID: " << getpid() << "  SysClockS: " << secs << "  SysClockNano " << nanos << "  \nPage Table:\n\tOwner PID\tPage Number\t2nd Chance Bit\tDirty Bit\n";
         for(int i = 0; i < FRAME_TABLE_SIZE; i++){
-            std::cout << "Frame " << std::to_string(i + 1) << ":\t" << std::to_string(frameTable[i].pid) << "\t" << std::to_string(frameTable[i].pageNumber) << "\t\t" << std::to_string(frameTable[i].secondChanceBit) << "\t" << std::to_string(frameTable[i].dirtyBit) << std::endl;
-            outputFile << "Frame " << std::to_string(i + 1) << ":\t" << std::to_string(frameTable[i].pid) << "\t" << std::to_string(frameTable[i].pageNumber) << "\t\t" << std::to_string(frameTable[i].secondChanceBit) << "\t" << std::to_string(frameTable[i].dirtyBit) << std::endl;
+            std::cout << "Frame " << std::to_string(i + 1) << ":\t" << std::to_string(frameTable[i].pid) << "\t" << std::to_string(frameTable[i].pageNumber) << "\t\t" << std::to_string(frameTable[i].secondChanceBit) << "\t\t" << std::to_string(frameTable[i].dirtyBit) << std::endl;
+            outputFile << "Frame " << std::to_string(i + 1) << ":\t" << std::to_string(frameTable[i].pid) << "\t" << std::to_string(frameTable[i].pageNumber) << "\t\t" << std::to_string(frameTable[i].secondChanceBit) << "\t\t" << std::to_string(frameTable[i].dirtyBit) << std::endl;
         }
         next_print_nanos = next_print_nanos + 500000000;
         if (next_print_nanos >= 1000000000){   // if over 1 billion nanos, add 1 second, sub 1 bil nanos
@@ -70,9 +70,10 @@ void print_frame_table(Page frameTable[], int secs, int nanos, std::ostream& out
     }
 }
 
-void page_fault(Page frameTable[], std::ofstream* outputFile, pid_t pid, int pageNumber, int msgCode){
+void page_fault(Page frameTable[], std::ofstream* outputFile, Clock* c, pid_t pid, int memoryAddress, int msgCode){
     static int victimFrame = 0;
     bool victimFound = false;
+    int pageNumber = memoryAddress/1024;  // translation of memory address
 
     while(!victimFound){
         if(frameTable[victimFrame].secondChanceBit == 1){   // victim not found
@@ -80,9 +81,13 @@ void page_fault(Page frameTable[], std::ofstream* outputFile, pid_t pid, int pag
         } else {    // second chance bit == 0, victim is found, swap out page
             victimFound = true;
             if(frameTable[victimFrame].dirtyBit){    // SAVE OLD PAGE BEFORE SWAPPING OUT
-                std::cout << "OSS: Swapping out dirty frame, saving to secondary storage..." << std::endl;
-                *outputFile << "OSS: Swapping out dirty frame, saving to secondary storage..." << std::endl;
+                std::cout << "OSS: Swapping out dirty frame, saving to secondary storage... Adding time to clock" << std::endl;
+                *outputFile << "OSS: Swapping out dirty frame, saving to secondary storage... Adding time to clock" << std::endl;
+                increment(c, 100);
             }
+            
+            std::cout << "OSS: Clearing frame " << victimFrame << " and swapping in Process " << pid << " page " << pageNumber << std::endl;
+            *outputFile << "OSS: Clearing frame " << victimFrame << " and swapping in Process " << pid << " page " << pageNumber << std::endl;
 
             frameTable[victimFrame].pid = pid;   // WRITING NEW PAGE TO MAIN MEMORY
             frameTable[victimFrame].pageNumber = pageNumber;
@@ -95,6 +100,14 @@ void page_fault(Page frameTable[], std::ofstream* outputFile, pid_t pid, int pag
             buf.memoryAddress = pageNumber;
             buf.msgCode = MSG_TYPE_GRANTED;
             send_msg_to_child(buf);
+
+            if(msgCode == MSG_TYPE_WRITE){
+                std::cout << "OSS: Address " << memoryAddress << " in frame " << victimFrame+1 << ", writing data to " << pid << " at time " << c->secs << ":" << c->nanos << std::endl;
+            *outputFile << "OSS: Address " << memoryAddress << " in frame " << victimFrame+1 << ", writing data to " << pid << " at time " << c->secs << ":" << c->nanos << std::endl;
+            } else {
+                std::cout << "OSS: Address " << memoryAddress << " in frame " << victimFrame+1 << ", giving data to " << pid << " at time " << c->secs << ":" << c->nanos << std::endl;
+            *outputFile << "OSS: Address " << memoryAddress << " in frame " << victimFrame+1 << ", giving data to " << pid << " at time " << c->secs << ":" << c->nanos << std::endl;
+            }            
         }
         victimFrame++;  // we increment victimFrame whether it's found or not
         if(victimFrame == FRAME_TABLE_SIZE){
@@ -112,17 +125,28 @@ void page_request(Page frameTable[], std::ofstream* outputFile, Clock* c, pid_t 
     int pageNumber = memoryAddress/1024;  // translation of memory address
     // int offset = memoryAddress % 1024;        
 
+    if(msgCode == MSG_TYPE_WRITE){
+        std::cout << "OSS: Process " << pid << " requesting read of address " << memoryAddress << " at time " << c->secs << ":" << c->nanos << std::endl;
+        *outputFile << "OSS: Process " << pid << " requesting read of address " << memoryAddress << " at time " << c->secs << ":" << c->nanos << std::endl;
+    } else {
+        std::cout << "OSS: Process " << pid << " requesting write of address " << memoryAddress << " at time " << c->secs << ":" << c->nanos << std::endl;
+        *outputFile << "OSS: Process " << pid << " requesting write of address " << memoryAddress << " at time " << c->secs << ":" << c->nanos << std::endl;
+    }
+
     for (int i = 0; i < FRAME_TABLE_SIZE; i++){  // init scan of frameTable
         if(frameTable[i].pid == pid && frameTable[i].pageNumber == pageNumber){  
             
             if(msgCode == MSG_TYPE_WRITE)
-                frameTable[i].dirtyBit = 1;     
+                frameTable[i].dirtyBit = 1;             
             frameTable[i].secondChanceBit = 1;       
             increment(c, 100);            
 
             buf.msgCode = MSG_TYPE_GRANTED;
             instantMemoryAccesses++;
             send_msg_to_child(buf); 
+            
+            std::cout << "OSS: Process " << pid << " Memory request instantly granted at time " << c->secs << ":" << c->nanos << std::endl;
+            *outputFile << "OSS: Process " << pid << " Memory request instantly granted at time " << c->secs << ":" << c->nanos << std::endl;
             return;
         }
     }
@@ -130,13 +154,16 @@ void page_request(Page frameTable[], std::ofstream* outputFile, Clock* c, pid_t 
     buf.msgCode = MSG_TYPE_BLOCKED;  
     send_msg_to_child(buf); 
 
+    std::cout << "OSS: Process " << pid << " Address " << memoryAddress << " not in a frame, pagefault" << std::endl;
+    *outputFile << "OSS: Process " << pid << " Address " << memoryAddress << " not in a frame, pagefault" << std::endl;
+
     // init time values
     int unblockSecs = c->secs;
     int unblockNanos = c->nanos;
     // add_time()
     add_time(&unblockSecs, &unblockNanos, 1.4e7);
 
-    blockedQueue.push(ProcInfo(pid, unblockSecs, unblockNanos, pageNumber, msgCode));
+    blockedQueue.push(ProcInfo(pid, unblockSecs, unblockNanos, memoryAddress, msgCode));
 
         // can't run page_fault() to replace until secondary storage retrieves
         // desired frame which is simulated by blocked queue and 14ms wait
@@ -149,7 +176,7 @@ void attempt_process_unblock(Page frameTable[], std::ofstream* outputFile, Clock
     while(!blockedQueue.empty() && (c->secs, c->nanos, blockedQueue.front().secs, blockedQueue.front().nanos)){
         std::cout << "OSS: Process " << blockedQueue.front().pid << " Unblocked and now swapping memory in" << std::endl; 
         *outputFile << "OSS: Process " << blockedQueue.front().pid << " Unblocked and now swapping memory in" << std::endl; 
-        page_fault(frameTable, outputFile, blockedQueue.front().pid, blockedQueue.front().pageNumber, blockedQueue.front().msgCode);    
+        page_fault(frameTable, outputFile, c, blockedQueue.front().pid, blockedQueue.front().pageNumber, blockedQueue.front().msgCode);    
         pageFaults++;  
         blockedQueue.pop();
     }
